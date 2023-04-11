47472cf028ad4998ef3ec4108aae06547dff2003
Rogers Mwangi Irungu <124586315+Irungu-Rogers04@users.noreply.github.com>
2023-04-11 19:31:04 +0300

    Add files via upload

---
+++ example.php
@@ -0,0 +1,135 @@
+<?php
+include("conn.php");
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+      /* Add CSS styles here */
+      /* Add CSS styles here */
+      body {
+        font-family: Arial, sans-serif;
+        font-size: 16px;
+        
+       
+        background-position: center;
+  
+        color: #fff;
+        line-height: 1.5;
+        margin: 0;
+        padding: 0;
+      }
+     
+      h1 {
+        margin: 0;
+        font-size: 36px;
+      }
+      main {
+        margin: 20px;
+      }
+      nav ul li a{
+        color:greenyellow;
+      }
+      h2 {
+        font-size: 24px;
+      }
+      ul {
+        list-style: none;
+        padding: 0;
+        margin: 0;
+      }
+      li {
+        margin: 20px 0;
+        
+      }
+      .image-box {
+        text-align: left;
+        margin-top: 20px;
+      }
+      img {
+        max-width: 100%;
+        height: auto;
+      }
+      footer {
+    background-color: #f2f2f2;
+    text-align: center;
+    padding: 10px;
+    color:black;
+  }
+    </style>
+  </head>
+  <body>
+
+    <main>
+        <section id="description">
+        
+            <p>
+             <br>
+             <br> 
+             <br>
+              <br>
+            </p>
+            <div class="image-box">
+              <img src="images/1page-img.jpg" alt="" />
+            </div>
+      <section id="options">
+
+        <ul>
+          <li>
+            <p>
+
+            </p>
+            <h3>
+
+            </h3>
+            <p>
+            
+            </p>
+          </li>
+          <li>
+            <h3>
+
+            </h3>
+            <p>
+              
+              <br>
+              
+              <br>
+            </p>
+          </li>
+          <li>
+            <h3>
+
+            </h3>
+            <p>
+              
+              <br>
+              
+              <br>
+            </p>
+          </li>
+        </ul>
+      </section>
+      <div class="img-box">
+
+      </div>
+      
+      <nav>
+        <ul>
+          <li><a href="Homepage.php">Home</a></li>
+          <li><a href="about us.php">About Us</a></li>
+          <li><a href="contactus.php">Contact Us</a></li>
+          <li><a href="faqs.html">FAQs</a></li>
+        </ul>
+      </nav>
+    </main>
+    <footer>
+  <p>&copy; 2023 Vehicle License Registration System. All rights reserved.</p>
+</footer>
+</body>
+</html>
