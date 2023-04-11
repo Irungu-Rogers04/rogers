85fb85427abeb5ecf20d02a4baa68043a046a895
Rogers Mwangi Irungu <124586315+Irungu-Rogers04@users.noreply.github.com>
2023-04-11 19:46:57 +0300

    Add files via upload

---
+++ users_messages.php
@@ -0,0 +1,56 @@
+<?php
+session_start();
+include("conn.php");
+
+// Query the database for all messages
+$query = "SELECT * FROM contact_us";
+$result = mysqli_query($conn, $query);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Users - Messages</title>
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        th, td {
+            text-align: left;
+            padding: 8px;
+        }
+
+        tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
+
+        th {
+            background-color: #4CAF50;
+            color: white;
+        }
+    </style>
+</head>
+<body>
+    <h1>Users - Messages</h1>
+    <table>
+        <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Message</th>
+        </tr>
+        <?php while ($row = mysqli_fetch_assoc($result)) { ?>
+            <tr>
+                <td><?php echo $row['name']; ?></td>
+                <td><?php echo $row['email']; ?></td>
+                <td><?php echo $row['message']; ?></td>
+            </tr>
+        <?php } ?>
+    </table>
+</body>
+</html>
