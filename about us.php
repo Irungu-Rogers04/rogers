43218cae972c2348b71817df830bbcd41eb860bf
Rogers Mwangi Irungu <124586315+Irungu-Rogers04@users.noreply.github.com>
2023-04-11 19:26:25 +0300

    Add files via upload

---
+++ about us.php
@@ -0,0 +1,78 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us</title>
+    <link rel="stylesheet" href="stylevehicle.css">
+    <style>
+        body {
+            background-image: url('slide3.jpg');
+            background-size: cover;
+            background-repeat: no-repeat;
+        }
+        
+        h1 {
+            color: #00bbff;
+            text-align: center;
+            font-size: 3rem;
+            margin-top: 2rem;
+        }
+        
+        p {
+            color: #7aff00;
+            font-size: 1.5rem;
+            text-align: center;
+            margin-top: 2rem;
+            line-height: 1.5;
+        }
+        
+        a {
+            display: inline-block;
+            background-color: #2200ff;
+            color: #fff;
+            font-size: 1.5rem;
+            padding: 1rem 2rem;
+            text-align: center;
+            margin-top: 2rem;
+            border-radius: 2rem;
+            transition: background-color 0.3s ease;
+        }
+        
+        a:hover {
+            background-color: #e8a21e;
+        }
+        
+        footer {
+            background-color: #333;
+            color: #fff;
+            padding: 2rem;
+            text-align: center;
+        }
+        
+        footer p {
+            font-size: 1.2rem;
+        }
+    </style>
+</head>
+<body>
+  
+    <main>
+        <h1>About us</h1>
+        <p>We are a motor vehicle registration and reporting system that 
+          offers easy and hassle-free services for vehicle registration and stolen 
+          vehicle reporting. Our experts are available to assist you with any queries, 
+          and we prioritize the security of your personal information. Our goal is to 
+          provide excellent customer service and ensure your experience with us is smooth and stress-free.</p>
+        <a href="log.php">Get started</a>
+    </main>
+    
+    <footer>
+        <p>&copy; 2023 Vehicle License Registration System. All rights reserved.</p>
+    </footer>
+</body>
+</html>
